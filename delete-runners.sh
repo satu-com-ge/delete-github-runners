de938e3eed3611deeab9ed70b0467aa8f6c02338
chago1991 <75668186+chago1991@users.noreply.github.com>
2024-01-30 11:56:13 +0400

    Update delete-runners.sh

--- delete-runners.sh
+++ delete-runners.sh
@@ -3,12 +3,12 @@
 REPO=$1
 TOKEN=$2
 
-RUNNER_LIST=$(curl -H "Authorization: token ${TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners | jq '[.runners[] | select(.status | contains("offline")) | {id: .id}]')
+RUNNER_LIST=$(curl -H "Authorization: Bearer ${TOKEN}" -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/orgs/${REPO}/actions/runners | jq '[.runners[] | select(.status | contains("offline")) | {id: .id}]')
 
 for id in $(echo "$RUNNER_LIST" | jq -r '.[] | @base64'); do
         _jq() {
                 echo ${id} | base64 --decode | jq -r ${1}
         }
         echo $(_jq '.id')
-        curl -X DELETE -H "Accept: application/vnd.github+json" -H "Authorization: token ${TOKEN}"  https://api.github.com/repos/${REPO}/actions/runners/$(_jq '.id')
+        curl -X DELETE -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/orgs/ORG/actions/runners/$(_jq '.id')
 done
